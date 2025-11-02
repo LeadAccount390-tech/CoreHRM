2bbc5713bb95c74e7f77fde6d54c8d0bfcb1ebb2
mounika <mounika@LAPTOP-J4LOL13O>
2025-11-02 21:18:52 +0530

    Worked on review comments.

--- ecom.project/src/test/java/com/Tests/TestPIMpage.java
+++ ecom.project/src/test/java/com/Tests/TestPIMpage.java
@@ -16,5 +16,6 @@ public class TestPIMpage extends Basepage {
 	  loginPage.login();
 	  TestNgutil.assertTrue(Commonutils.getElementText(loginPage.getWelcomepage(),"Welcome selenium"));
 	  System.out.println("wlcome to github");
+	  System.out.println("wlcome to github");
   }
 }
